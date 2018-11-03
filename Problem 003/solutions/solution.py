c40a104dc51700eecbe93bea18d7ca97e365fd85
Lex <lexvonbon@users.noreply.github.com>
2018-11-02 21:29:03 -0500

    Partial Solution 3 - v4

--- Problem 003/solutions/solution.py
+++ Problem 003/solutions/solution.py
@@ -4,11 +4,20 @@ line = input()
 n = int(n)
 m = int(m)
 
-relatedStructs = []
+isA, hasA = [], []
 
 for i in range(n):
-    print(input(), " : R")
+    rel = input().split(' ')
+    if (rel[1] == "is-a"):
+        isA.append((rel[0], rel[2]))
+    elif (rel[1] == "has-a"):
+        hasA.append((rel[0], rel[2]))
 
 
 for i in range(m):
-    print(input(), " : Q")
+    que = input().split(' ')
+    if (que[1] == "is-a"):
+        for j in isA:
+            if (rel[0], rel[2]) = isA()
+    elif (que[1] == "has-a"):
+        hasA.append((rel[0], rel[2]))
