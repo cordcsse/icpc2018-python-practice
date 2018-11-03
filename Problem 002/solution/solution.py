757c01ae87acb45ae5bf2b44d8f056ab893d6628
Lex <lexvonbon@users.noreply.github.com>
2018-11-02 20:03:49 -0500

    Partial Solution 2 - v2

---
+++ Problem 002/solution/solution.py
@@ -0,0 +1,18 @@
+storeNum = int(input())
+
+stores = []
+
+for i in range(storeNum):
+    line = input().split(" ")
+    stores.append(list(line))
+
+count = 0
+seaLevel = 0
+
+
+# for i in range(storeNum):
+  #  min(stores[])
+
+
+
+print(stores)
